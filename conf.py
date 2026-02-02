c2c16dc124299d7903d1860a86659f30ce4b53fe
Christina Adair <79288082+ChristinaAdair@users.noreply.github.com>
2026-02-02 10:36:30 -0800

    Add linkcheck_ignore for specific URL

--- conf.py
+++ conf.py
@@ -2,3 +2,8 @@
 
 from documenteer.conf.guide import *  # noqa: F401, F403
 nb_execution_mode = 'off'
+
+linkcheck_ignore = [
+    r'https://www\.lsst\.org/content/lsst-statement-regarding-increased-deployment-satellite-constellations',
+]
+
