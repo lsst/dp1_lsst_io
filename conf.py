ef274e80c9596f0b654ed1a76235a8524f1d33d7
Christina Adair <79288082+ChristinaAdair@users.noreply.github.com>
2026-02-02 10:57:13 -0800

    Refactor linkcheck_ignore entries in conf.py

--- conf.py
+++ conf.py
@@ -3,11 +3,8 @@
 from documenteer.conf.guide import *  # noqa: F401, F403
 nb_execution_mode = 'off'
 
+
 linkcheck_ignore = [
     r'https://www\.lsst\.org/content/lsst-statement-regarding-increased-deployment-satellite-constellations',
-]
-
-linkcheck_ignore = [
     r'https://healpix\.sourceforge\.io/?',
 ]
-
